cdecc0cc479c6d41e1aaf7513ed81eb58c33fe01
irunwazed <irunwazed@gmail.com>
2022-08-09 07:19:21 +0800

    import Renstra

--- app/Imports/ImportContoh.php
+++ app/Imports/ImportContoh.php
@@ -15,7 +15,7 @@ class ImportContoh implements ToModel
      */
 
 
-    private $urusan_kode, $bidang_kode, $program_kode, $kegiatan_kode, $sub_kegiatan_kode, $data, $opd, $sumber_data, $lokasi;
+    private $no, $urusan_kode, $bidang_kode, $program_kode, $kegiatan_kode, $opd_id, $uraian, $level;
     public function __construct()
     {
         $this->data = [];
@@ -28,115 +28,177 @@ class ImportContoh implements ToModel
 
 
 
-			echo "<pre>";
-			print_r($row);
-			echo "</pre>";
+                
+        
+        $data = [];
+        $where = [];
 
-        $simpan = false;
-        if (
-            is_numeric($row[0])
-            && is_numeric($row[1])
-            && is_numeric($row[2])
-            && is_numeric($row[3])
-            && is_numeric($row[4])
-        ) {
-            $this->urusan_kode = $row[0];
-            $this->bidang_kode = $row[1];
-            $this->program_kode = $row[2];
-            $this->kegiatan_kode = @$row[3][0] . @$row[3][2] . @$row[3][3];
-            $this->sub_kegiatan_kode = $row[4];
 
-            if($this->program_kode == 1){
-                $this->urusan_kode = 0;
-                $this->bidang_kode = 0;
+        if(
+            !($row[0] == ''
+            && $row[1] == ''
+            && $row[2] == ''
+            && $row[3] == ''
+            && $row[4] == ''
+            && $row[5] == '')
+        ){
+
+            //program kegiatan
+            if($row[2] != ''){
+                $kode = explode(".", $row[2]);
+                $this->urusan_kode = $kode[0];
+                $this->bidang_kode = $kode[1];
+                $this->program_kode = $kode[2];
+                $this->opd_id = $row[19];
+                
+                $data['urusan_kode'] = $this->urusan_kode;
+                $data['bidang_kode'] = $this->bidang_kode;
+                $data['program_kode'] = $this->program_kode;
+                $data['opd_id'] = $this->opd_id;
+                $where = $data;
+
+                
+                $data['uraian'] = $row[3];
+                $data['uraian'] = $row[3];
+                $data['indikator'] = $row[4];
+                
+                if(count($kode) == 3){
+                    //program
+                    $data['level'] = 3;
+                    
+                    $dataRPJMD = DB::table('ref_rpjmd_program')->where($where)->get();
+                    if(count($dataRPJMD) > 0){
+
+                    }else{
+                        echo "<pre>";
+                        // print_r($dataRPJMD);
+                        print_r($row);
+                        echo "</pre>";
+                    }
+
+                }else if(count($kode) == 5){
+                    // kegiatan
+                    $this->kegiatan_kode = $kode[3].".".$kode[4];
+                    $data['kegiatan_kode'] = $this->kegiatan_kode;
+                    $data['level'] = 4;
+
+                }
+    
+
+                // ->where('ref_rpjmd_program.urusan_kode', $this->urusan_kode)
+                // ->where('ref_rpjmd_program.bidang_kode', $this->bidang_kode)
+                // ->where('ref_rpjmd_program.program_kode', $this->program_kode)
+                // ->where('ref_rpjmd_program.opd_id', $this->opd_id)
             }
+
+        }
+
+
+        // $simpan = false;
+        // if (
+        //     is_numeric($row[0])
+        //     && is_numeric($row[1])
+        //     && is_numeric($row[2])
+        //     && is_numeric($row[3])
+        //     && is_numeric($row[4])
+        // ) {
+        //     $this->urusan_kode = $row[0];
+        //     $this->bidang_kode = $row[1];
+        //     $this->program_kode = $row[2];
+        //     $this->kegiatan_kode = @$row[3][0] . @$row[3][2] . @$row[3][3];
+        //     $this->sub_kegiatan_kode = $row[4];
+
+        //     if($this->program_kode == 1){
+        //         $this->urusan_kode = 0;
+        //         $this->bidang_kode = 0;
+        //     }
             
-            $this->lokasi = (string)$row[9];
-            $this->sumber_data = (string)$row[10];
-            $this->opd = (int)$row[11];
+        //     $this->lokasi = (string)$row[9];
+        //     $this->sumber_data = (string)$row[10];
+        //     $this->opd = (int)$row[11];
 
-            if (
-                is_numeric($this->urusan_kode)
-                && is_numeric($this->bidang_kode)
-                && is_numeric($this->program_kode)
-                && is_numeric($this->kegiatan_kode)
-                && is_numeric($this->sub_kegiatan_kode)
-                && $row[6] != ''
-            ) {
+        //     if (
+        //         is_numeric($this->urusan_kode)
+        //         && is_numeric($this->bidang_kode)
+        //         && is_numeric($this->program_kode)
+        //         && is_numeric($this->kegiatan_kode)
+        //         && is_numeric($this->sub_kegiatan_kode)
+        //         && $row[6] != ''
+        //     ) {
 
-                $simpan = true;
+        //         $simpan = true;
 
-            }
-        } else if (
-            $row[0] == ''
-            && $row[6] != ''
-            && is_numeric(@$this->urusan_kode)
-        ) {
+        //     }
+        // } else if (
+        //     $row[0] == ''
+        //     && $row[6] != ''
+        //     && is_numeric(@$this->urusan_kode)
+        // ) {
 
-            $simpan = true;
-        }
+        //     $simpan = true;
+        // }
 
-        if ($simpan) {
+        // if ($simpan) {
 
-            $volume = str_replace("\xc2\xa0", ' ', $row[7]);
-            $volumeArr = explode(" ", $volume);
-            $target = (int)@$volumeArr[0];
-            array_shift($volumeArr);
-            $satuan = implode(" ", $volumeArr);
+        //     $volume = str_replace("\xc2\xa0", ' ', $row[7]);
+        //     $volumeArr = explode(" ", $volume);
+        //     $target = (int)@$volumeArr[0];
+        //     array_shift($volumeArr);
+        //     $satuan = implode(" ", $volumeArr);
 
-            $dataSub = [
-                'tahun_ke' => session('tahun'),
-                'opd_id' => session('opd'), //$this->opd,
-                'permen_ver' => 1,
-                'urusan_kode' => $this->urusan_kode,
-                'bidang_kode' => $this->bidang_kode,
-                'program_kode' => $this->program_kode,
-                'kegiatan_kode' => $this->kegiatan_kode,
-                'sub_kegiatan_kode' => $this->sub_kegiatan_kode,
-            ];
+        //     $dataSub = [
+        //         'tahun_ke' => session('tahun'),
+        //         'opd_id' => session('opd'), //$this->opd,
+        //         'permen_ver' => 1,
+        //         'urusan_kode' => $this->urusan_kode,
+        //         'bidang_kode' => $this->bidang_kode,
+        //         'program_kode' => $this->program_kode,
+        //         'kegiatan_kode' => $this->kegiatan_kode,
+        //         'sub_kegiatan_kode' => $this->sub_kegiatan_kode,
+        //     ];
 
-            $subId = DB::table('ref_rkpd_sub_kegiatan')->where($dataSub)->first();
-            if(@$subId->id){
-                $subId = $subId->id;
-            }else{
-                $subId = DB::table('ref_rkpd_sub_kegiatan')->insertGetId($dataSub);
-            }
+        //     $subId = DB::table('ref_rkpd_sub_kegiatan')->where($dataSub)->first();
+        //     if(@$subId->id){
+        //         $subId = $subId->id;
+        //     }else{
+        //         $subId = DB::table('ref_rkpd_sub_kegiatan')->insertGetId($dataSub);
+        //     }
 
-            $dataIndi = [
-                'rkpd_sub_kegiatan_id' => $subId,
-                'rkpd_sub_kegiatan_indikator_nama' => (string)$row[6],
-                'rkpd_sub_kegiatan_indikator_nilai_jenis' => 1,
-                'rkpd_sub_kegiatan_indikator_nilai_json' => '[]',
-                'rkpd_sub_kegiatan_indikator_target' => $target,
-                'rkpd_sub_kegiatan_indikator_satuan' => $satuan,
-                'rkpd_sub_kegiatan_indikator_pagu' => (float)$row[8],
-                'rkpd_sub_kegiatan_indikator_pagu_perubahan' => (float)$row[8],
-                'rkpd_sub_kegiatan_indikator_lokasi' => $this->lokasi,
-            ];
+        //     $dataIndi = [
+        //         'rkpd_sub_kegiatan_id' => $subId,
+        //         'rkpd_sub_kegiatan_indikator_nama' => (string)$row[6],
+        //         'rkpd_sub_kegiatan_indikator_nilai_jenis' => 1,
+        //         'rkpd_sub_kegiatan_indikator_nilai_json' => '[]',
+        //         'rkpd_sub_kegiatan_indikator_target' => $target,
+        //         'rkpd_sub_kegiatan_indikator_satuan' => $satuan,
+        //         'rkpd_sub_kegiatan_indikator_pagu' => (float)$row[8],
+        //         'rkpd_sub_kegiatan_indikator_pagu_perubahan' => (float)$row[8],
+        //         'rkpd_sub_kegiatan_indikator_lokasi' => $this->lokasi,
+        //     ];
 
 
-            // $status = DB::table('ref_rkpd_sub_kegiatan_indikator')->insert($dataIndi);
+        //     // $status = DB::table('ref_rkpd_sub_kegiatan_indikator')->insert($dataIndi);
 
-            // echo "<pre>";
-            // print_r([
-            //     'urusan_kode' => $this->urusan_kode,
-            //     'bidang_kode' => $this->bidang_kode,
-            //     'program_kode' => $this->program_kode,
-            //     'kegiatan_kode' => $this->kegiatan_kode,
-            //     'sub_kegiatan_kode' => $this->sub_kegiatan_kode,
-            //     'uraian' => $row[5],
-            //     'indikator' => $row[6],
-            //     'target' => @$target,
-            //     'satuan' => @$satuan,
-            //     'pagu' => (float)@$row[8],
-            //     'lokasi' => $this->lokasi,
-            //     'sumber_data' => $this->sumber_data,
-            //     'opd' => $this->opd,
-            //     'tahun' => session('tahun'),
-            // ]);
-            // // print_r($row);
-            // echo "</pre>";
-        }
+        //     // echo "<pre>";
+        //     // print_r([
+        //     //     'urusan_kode' => $this->urusan_kode,
+        //     //     'bidang_kode' => $this->bidang_kode,
+        //     //     'program_kode' => $this->program_kode,
+        //     //     'kegiatan_kode' => $this->kegiatan_kode,
+        //     //     'sub_kegiatan_kode' => $this->sub_kegiatan_kode,
+        //     //     'uraian' => $row[5],
+        //     //     'indikator' => $row[6],
+        //     //     'target' => @$target,
+        //     //     'satuan' => @$satuan,
+        //     //     'pagu' => (float)@$row[8],
+        //     //     'lokasi' => $this->lokasi,
+        //     //     'sumber_data' => $this->sumber_data,
+        //     //     'opd' => $this->opd,
+        //     //     'tahun' => session('tahun'),
+        //     // ]);
+        //     // // print_r($row);
+        //     // echo "</pre>";
+        // }
 
         
 
